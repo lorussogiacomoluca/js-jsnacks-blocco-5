1444194c85dab7c0373d66594b5550eff2c00de3
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-04-02 15:49:55 +0200

    [UPD] Snack #5

--- snack-5/script.js
+++ snack-5/script.js
@@ -2,3 +2,8 @@ const nums = [2, 8, 4, 7, 12, 87];
 
 // Crea un array a partire dall'array nums, che abbia solo i numeri pari
 // Risultato: [2, 8, 4, 12]
+
+console.log("-".repeat(50));
+
+const evenNum = nums.filter(item => item % 2 === 0)
+console.log(evenNum);
