c9a97d0c737582c71da82e44c68a0609a2fd8c95
lorussogiacomoluca <lorussogiacomoluca@gmail.com>
2025-04-02 15:38:49 +0200

    [UPD] Snack #1

--- snack-1/script.js
+++ snack-1/script.js
@@ -2,3 +2,12 @@ const names = ['Edoardo', 'Simone', 'Francesco'];
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
+
+console.log("-".repeat(50));
+//for
+for(let i = 0; i < names.length; i++)
+    console.log(names[i]);
+
+console.log("-".repeat(50));
+//forEach
+names.forEach(item => console.log(item))
